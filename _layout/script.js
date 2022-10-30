99a501362c3a6293ee6395e8edcbbde404c4faaa
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2022-10-29 23:44:11 -0300

    fix

--- _layout/script.js
+++ _layout/script.js
@@ -7,3 +7,7 @@ message.classList.add("cookie-message");
 message.innerHTML = `We use cookies for improved functionality and analytics. <button class="btn-close-cookie"> Got it! </button>`;
 
 body.append(message);
+
+document.querySelector(".btn-close-cookie").addEventListener("click", function () {
+   message.remove();
+});
