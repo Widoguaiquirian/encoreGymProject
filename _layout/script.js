855443c69bb6ca013dbc28e41710913209c95528
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2022-10-29 23:40:23 -0300

    several advances

--- _layout/script.js
+++ _layout/script.js
@@ -1,3 +1,9 @@
 "use strict";
-// ! FOR LESSONS NAMES //
-// ? FOR REGULAR COMMENTS //
+const body = document.querySelector(".body");
+const message = document.createElement("div");
+
+// Cookie messages
+message.classList.add("cookie-message");
+message.innerHTML = `We use cookies for improved functionality and analytics. <button class="btn-close-cookie"> Got it! </button>`;
+
+body.append(message);
