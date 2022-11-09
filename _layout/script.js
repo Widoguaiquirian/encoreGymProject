7daa9b598c7d73fcab576162833e8e4113fda7d4
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2022-11-09 08:41:10 -0300

    New features

--- _layout/script.js
+++ _layout/script.js
@@ -11,3 +11,29 @@
 // document.querySelector(".btn-close-cookie").addEventListener("click", function () {
 //    message.remove();
 // });
+
+//Crowd meter
+//Clase que comparten los botones
+const tabs = document.querySelectorAll(".tab-day");
+//Clase del container padre de los botones
+const tabsContainer = document.querySelector(".tabs-component-container");
+//Clase que comparten los bloques de HTML a mostrar
+const tabsContent = document.querySelectorAll(".tabs-content");
+
+// ? AGREGAMOS EVENTO AL PADRE DE LOS BOTONES //
+tabsContainer.addEventListener("click", function (e) {
+   const clicked = e.target.closest(".tab-day");
+   if (!clicked) return;
+
+   tabs.forEach(function (t) {
+      t.classList.remove("tabs-button-active");
+   });
+
+   tabsContent.forEach(function (c) {
+      c.classList.remove("tabs-content-active");
+   });
+
+   clicked.classList.add("tabs-button-active");
+
+   document.querySelector(`.tabs-content-${clicked.dataset.tab}`).classList.add("tabs-content-active");
+});
