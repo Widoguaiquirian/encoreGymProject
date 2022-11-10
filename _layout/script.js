fa6d3365d470a32433acccf2c5a0291b0f3f7504
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2022-11-10 07:09:06 -0300

    fix

--- _layout/script.js
+++ _layout/script.js
@@ -1,1 +1,39 @@
 "use strict";
+// const body = document.querySelector(".body");
+// const message = document.createElement("div");
+
+// // Cookie messages
+// message.classList.add("cookie-message");
+// message.innerHTML = `We use cookies for improved functionality and analytics. <button class="btn-close-cookie"> GOT IT! </button>`;
+
+// body.append(message);
+
+// document.querySelector(".btn-close-cookie").addEventListener("click", function () {
+//    message.remove();
+// });
+
+//Crowd meter
+//Clase que comparten los botones
+const tabsDays = document.querySelectorAll(".tab-day");
+//Clase del container padre de los botones
+const tabsDaysContainer = document.querySelector(".box-top");
+//Clase que comparten los bloques de HTML a mostrar
+const contentBoxMedium = document.querySelectorAll(".box-medium");
+
+// ? AGREGAMOS EVENTO AL PADRE DE LOS BOTONES //
+tabsDaysContainer.addEventListener("click", function (e) {
+   const clicked = e.target.closest(".tab-day");
+   if (!clicked) return;
+
+   tabsDays.forEach(function (t) {
+      t.classList.remove("activeDay");
+   });
+
+   contentBoxMedium.forEach(function (c) {
+      c.classList.remove("grid-active");
+   });
+
+   clicked.classList.add("activeDay");
+
+   document.querySelector(`.grid-bar-${clicked.dataset.tab}`).classList.add("grid-active");
+});
