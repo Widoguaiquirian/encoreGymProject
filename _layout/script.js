26fba629cbe893eab7b4b7b5980f3b6d210a1009
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2022-11-12 16:31:11 -0300

    Footer done

--- _layout/script.js
+++ _layout/script.js
@@ -1,16 +1,16 @@
 "use strict";
-const body = document.querySelector(".body");
-const message = document.createElement("div");
+// const body = document.querySelector(".body");
+// const message = document.createElement("div");
 
-// Cookie messages
-message.classList.add("cookie-message");
-message.innerHTML = `We use cookies for improved functionality and analytics. <button class="btn-close-cookie"> GOT IT! </button>`;
+// // Cookie messages
+// message.classList.add("cookie-message");
+// message.innerHTML = `We use cookies for improved functionality and analytics. <button class="btn-close-cookie"> GOT IT! </button>`;
 
-body.append(message);
+// body.append(message);
 
-document.querySelector(".btn-close-cookie").addEventListener("click", function () {
-   message.remove();
-});
+// document.querySelector(".btn-close-cookie").addEventListener("click", function () {
+//    message.remove();
+// });
 
 //Crowd meter
 //Clase que comparten los botones
