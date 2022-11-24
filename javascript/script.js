8822f7e6c1ab7edb380539c0cdd0f5f4e3c208ab
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2022-11-23 22:32:40 -0300

    Applying BEM

--- javascript/script.js
+++ javascript/script.js
@@ -95,9 +95,9 @@ closeBtn.addEventListener("click", function () {
 
 // carrousel of experiences
 const sliderGrid = function () {
-   const slidesGrid = document.querySelectorAll(".slide-grid");
-   const btnLeftGrid = document.querySelector(".grid__btn--left");
-   const btnRightGrid = document.querySelector(".grid__btn--right");
+   const slidesGrid = document.querySelectorAll(".feed__slider");
+   const btnLeftGrid = document.querySelector(".feed_btn--left");
+   const btnRightGrid = document.querySelector(".feed_btn--right");
 
    let curSlideGrid = 0;
    // ? PARA QUE EL MAXIMO SEA EL NUMERO DE SLIDES USADOS //
