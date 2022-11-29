d0e0dc2ce0b531027eb5bb5bc23404ac997ff402
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2022-11-28 22:07:07 -0300

    Ordering JS files

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,26 @@
 "use strict";
 
+//! Contact modal
+const modal = document.querySelector(".modal");
+const btnsOpenModal = document.querySelectorAll(".modal__btn--open");
+const btnCloseModal = document.querySelector(".modal__btn--close");
+const overlay = document.querySelector(".modal__overlay");
+
+const openModal = function (e) {
+   e.preventDefault();
+   overlay.classList.remove("modal__hidden");
+   modal.classList.remove("modal__hidden");
+};
+
+const closeModal = function (e) {
+   e.preventDefault();
+   overlay.classList.add("modal__hidden");
+   modal.classList.add("modal__hidden");
+};
+
+btnCloseModal.addEventListener("click", closeModal);
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+
 //! Cookie messages
 const body = document.querySelector(".body");
 const message = document.createElement("div");
@@ -13,55 +34,23 @@ document.querySelector(".btn-ok-cookie").addEventListener("click", function () {
    message.remove();
 });
 
-//! Crowd meter
-document.addEventListener("DOMContentLoaded", () => {
-   //Clase que comparten los botones
-   const tabsDays = document.querySelectorAll(".crowd__tab-day");
-   //Clase del container padre de los botones
-   const tabsDaysContainer = document.querySelector(".schedule__inner-top");
-   //Clase que comparten los bloques de HTML a mostrar
-   const contentBoxMedium = document.querySelectorAll(".schedule__inner-medium");
+//! Login modal
+const loginModal = document.querySelector(".header__login-box");
+const hoverModalActive = document.querySelector(".header__link--login");
+const hoverModalDesactive = document.querySelector(".header__link--login");
 
-   // ? AGREGAMOS EVENTO AL PADRE DE LOS BOTONES //
+const openLoginModal = function (e) {
+   e.preventDefault();
+   loginModal.classList.remove("header__login-box-hide");
+};
 
-   tabsDaysContainer.addEventListener("click", function (e) {
-      const clicked = e.target.closest(".crowd__tab-day");
-      if (!clicked) return;
+const closeLoginModal = function (e) {
+   e.preventDefault();
+   loginModal.classList.add("header__login-box-hide");
+};
 
-      tabsDays.forEach(function (t) {
-         t.classList.remove("crowd__tab-day--active");
-      });
-
-      contentBoxMedium.forEach(function (c) {
-         c.classList.remove("group__bar--active");
-      });
-
-      clicked.classList.add("crowd__tab-day--active");
-
-      document.querySelector(`.group__bar-${clicked.dataset.tab}`).classList.add("group__bar--active");
-   });
-});
-
-//! Scroll revealing
-// const allRevealSections = document.querySelectorAll(".block");
-
-// const revealSection = function (entries, oberserver) {
-//    const [entry] = entries;
-
-//    if (!entry.isIntersecting) return;
-//    entry.target.classList.remove("block--hidden");
-//    oberserver.unobserve(entry.target);
-// };
-
-// const sectionOberserver = new IntersectionObserver(revealSection, {
-//    root: null,
-//    threshold: 0.1,
-// });
-
-// allRevealSections.forEach(function (section) {
-//    sectionOberserver.observe(section);
-//    section.classList.add("block--hidden");
-// });
+hoverModalActive.addEventListener("mouseover", openLoginModal);
+hoverModalDesactive.addEventListener("mouseout", closeLoginModal);
 
 //! Sticky nav
 const nav = document.querySelector(".header__nav");
@@ -93,95 +82,23 @@ closeBtn.addEventListener("click", function () {
    });
 });
 
-//! Carrousel of experience
-window.onload = function () {
-   const sliderGrid = function () {
-      const slidesGrid = document.querySelectorAll(".feed__slider");
-      const btnLeftGrid = document.querySelector(".feed_btn--left");
-      const btnRightGrid = document.querySelector(".feed_btn--right");
+//! Scroll revealing
+// const allRevealSections = document.querySelectorAll(".block");
 
-      let curSlideGrid = 0;
-      // ? PARA QUE EL MAXIMO SEA EL NUMERO DE SLIDES USADOS //
-      const maxSlideGrid = slidesGrid.length;
+// const revealSection = function (entries, oberserver) {
+//    const [entry] = entries;
 
-      const goToSlide = function (slide) {
-         slidesGrid.forEach((s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`));
-      };
+//    if (!entry.isIntersecting) return;
+//    entry.target.classList.remove("block--hidden");
+//    oberserver.unobserve(entry.target);
+// };
 
-      // Next slide
-      const nextSlide = function () {
-         if (curSlideGrid === maxSlideGrid - 1) {
-            curSlideGrid = 0;
-         } else {
-            curSlideGrid++;
-         }
+// const sectionOberserver = new IntersectionObserver(revealSection, {
+//    root: null,
+//    threshold: 0.1,
+// });
 
-         goToSlide(curSlideGrid);
-      };
-
-      const prevSlide = function () {
-         if (curSlideGrid === 0) {
-            curSlideGrid = maxSlideGrid - 1;
-         } else {
-            curSlideGrid--;
-         }
-         goToSlide(curSlideGrid);
-      };
-
-      const init = function () {
-         goToSlide(0);
-      };
-
-      init();
-
-      // Event handlers
-      btnRightGrid.addEventListener("click", nextSlide);
-      btnLeftGrid.addEventListener("click", prevSlide);
-   };
-
-   sliderGrid();
-};
-
-//! Contact modal
-window.onload = function () {
-   const modal = document.querySelector(".modal");
-   const btnsOpenModal = document.querySelectorAll(".modal__btn--open");
-   const btnCloseModal = document.querySelector(".modal__btn--close");
-   const overlay = document.querySelector(".modal__overlay");
-
-   const openModal = function (e) {
-      e.preventDefault();
-      overlay.classList.remove("modal__hidden");
-      modal.classList.remove("modal__hidden");
-   };
-
-   const closeModal = function (e) {
-      e.preventDefault();
-      overlay.classList.add("modal__hidden");
-      modal.classList.add("modal__hidden");
-   };
-
-   btnCloseModal.addEventListener("click", closeModal);
-
-   btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
-};
-
-//! Login modal
-window.onload = function () {
-   const loginModal = document.querySelector(".header__login-box");
-   const hoverModalActive = document.querySelector(".header__link--login");
-   const hoverModalDesactive = document.querySelector(".header__link--login");
-
-   const openLoginModal = function (e) {
-      e.preventDefault();
-      loginModal.classList.remove("header__login-box-hide");
-   };
-
-   const closeLoginModal = function (e) {
-      e.preventDefault();
-      loginModal.classList.add("header__login-box-hide");
-   };
-
-   hoverModalActive.addEventListener("mouseover", openLoginModal);
-   hoverModalDesactive.addEventListener("mouseout", closeLoginModal);
-};
+// allRevealSections.forEach(function (section) {
+//    sectionOberserver.observe(section);
+//    section.classList.add("block--hidden");
+// });
